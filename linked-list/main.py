946a4568520785f158c6296ae5dcc5a8ff98a7a9
pemidi <rashidi.peyman@gmail.com>
2024-04-02 05:16:32 +0330

    add print 🖨 and ➕ methods

--- linked-list/main.py
+++ linked-list/main.py
@@ -4,6 +4,7 @@ class Node:
         self.node = None
 
 
+# This Python class defines a linked list data structure with methods to initialize the list and print its elements.
 class LinkedList:
     def __init__(self, value):
         self.new_node = Node(value)
@@ -11,7 +12,27 @@ class LinkedList:
         self.tail = self.new_node
         self.length = 1
 
+    def print(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
 
 my_linked_list = LinkedList(4)
 
-print(my_linked_list.head.value)
+my_linked_list.append(6)
+my_linked_list.append(7)
+my_linked_list.append(8)
+
+print(my_linked_list.tail.value)
